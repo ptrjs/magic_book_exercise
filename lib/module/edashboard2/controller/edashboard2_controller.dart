e2b1088d56dced2e1fdd46c5f9e197d3bf9e1036
ptrjs <peterjose64@gmail.com>
2023-05-28 00:18:15 +0700

    membuat carousel edash 2

--- lib/module/edashboard2/controller/edashboard2_controller.dart
+++ lib/module/edashboard2/controller/edashboard2_controller.dart
@@ -1,8 +1,10 @@
+import 'package:carousel_slider/carousel_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:magic_book_exercise/state_util.dart';
 import '../view/edashboard2_view.dart';
 
-class Edashboard2Controller extends State<Edashboard2View> implements MvcController {
+class Edashboard2Controller extends State<Edashboard2View>
+    implements MvcController {
   static late Edashboard2Controller instance;
   late Edashboard2View view;
 
@@ -17,4 +19,7 @@ class Edashboard2Controller extends State<Edashboard2View> implements MvcControl
 
   @override
   Widget build(BuildContext context) => widget.build(context, this);
+
+  int currentIndex = 0;
+  final CarouselController carouselController = CarouselController();
 }
--- lib/module/edashboard2/view/edashboard2_view.dart
+++ lib/module/edashboard2/view/edashboard2_view.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:magic_book_exercise/core.dart';
 import '../controller/edashboard2_controller.dart';
@@ -9,15 +10,123 @@ class Edashboard2View extends StatefulWidget {
     controller.view = this;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Edashboard2"),
-        actions: const [],
-      ),
       body: SingleChildScrollView(
         child: Container(
-          padding: const EdgeInsets.all(10.0),
-          child: const Column(
-            children: [],
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Image.network(
+                    "https://icons.iconarchive.com/icons/aha-soft/desktop-buffet/256/Piece-of-cake-icon.png",
+                    width: 32.0,
+                    height: 32.0,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(
+                    width: 6.0,
+                  ),
+                  const Expanded(
+                    child: Text(
+                      "Food Store",
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const CircleAvatar(
+                    backgroundColor: Colors.grey,
+                    child: Icon(
+                      Icons.person,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              /*
+              TODO: Implement this @ controller
+              int currentIndex = 0;
+              final CarouselController carouselController = CarouselController();
+              */
+              Builder(builder: (context) {
+                List images = [
+                  "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+                  "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+                  "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=781&q=80",
+                  "https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
+                  "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=710&q=80",
+                ];
+
+                return Column(
+                  children: [
+                    CarouselSlider(
+                      carouselController: controller.carouselController,
+                      options: CarouselOptions(
+                        height: 110.0,
+                        autoPlay: true,
+                        enlargeCenterPage: true,
+                        onPageChanged: (index, reason) {
+                          controller.currentIndex = index;
+                          controller.setState(() {});
+                        },
+                      ),
+                      items: images.map((imageUrl) {
+                        return Builder(
+                          builder: (BuildContext context) {
+                            return Container(
+                              width: MediaQuery.of(context).size.width,
+                              margin:
+                                  const EdgeInsets.symmetric(horizontal: 5.0),
+                              decoration: BoxDecoration(
+                                color: Colors.amber,
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(6.0),
+                                ),
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                    imageUrl,
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      }).toList(),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: images.asMap().entries.map((entry) {
+                        return GestureDetector(
+                          onTap: () => controller.carouselController
+                              .animateToPage(entry.key),
+                          child: Container(
+                            width: 5.0,
+                            height: 5.0,
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 8.0, horizontal: 4.0),
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: (Theme.of(context).brightness ==
+                                            Brightness.dark
+                                        ? Colors.white
+                                        : Colors.black)
+                                    .withOpacity(
+                                        controller.currentIndex == entry.key
+                                            ? 0.9
+                                            : 0.4)),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                );
+              }),
+            ],
           ),
         ),
       ),
